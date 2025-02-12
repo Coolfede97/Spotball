bb13a961d7e70c844cdbfd1ef405a6c930b4c8e7
Coolfede97 <coolfede97@gmail.com>
2025-02-12 11:40:46 -0300

    switch terminado

--- Spotball/Assets/Scripts/SwitchingBlocks.cs
+++ Spotball/Assets/Scripts/SwitchingBlocks.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Android.Types;
 using UnityEngine;
 using static Unity.Collections.AllocatorManager;
-
+using System;
 public class SwitchingBlocks : MonoBehaviour
 {
     [Header("¡SOLO MODIFICAR SI ES FATHER!!!!!!")]
@@ -17,10 +18,16 @@ public class SwitchingBlocks : MonoBehaviour
     [SerializeField] [Range(0, 1)] float timeBeforeSwitchPercentage;
     [Header("¡NO MODIFICAR SI ES FATHER!!!!")]
     public int turn=0;
+    public string[] componentsToDisable;
     void Start()
     {
         if (father)
         {
+            if (numberOfBlinks % 2 != 0)
+            {
+                numberOfBlinks++;
+                Debug.Log("Acordate de poner un numero par en number of blinks");
+            }
             GetBlocksInTurns();
             for (int i = 1; i < turnsLength; i++)
             {
@@ -62,40 +69,80 @@ public class SwitchingBlocks : MonoBehaviour
         {
             yield return new WaitForSeconds(intervalTime-intervalTime*timeBeforeSwitchPercentage);
 
-            for (int i = 0; i < numberOfBlinks; i++) 
-            {
-                for (int j = 0; j < blocksInTurns.Count; j++) 
-                {
-                    foreach (SwitchingBlocks sb in blocksInTurns[j])
-                    {
-                        StartCoroutine(Blink(sb.GetComponent<SpriteRenderer>(), i % 2 == 0));
-                    }
-                }
-            }
             foreach (SwitchingBlocks block in blocksInTurns[currentTurn])
             {
+                if (blocksInTurns.Count==1 && block.gameObject.activeSelf == false) break;
                 StartCoroutine(Blink(block.GetComponent<SpriteRenderer>(), true));
             }
             currentTurn = currentTurn == turnsLength - 1 ? 0 : currentTurn + 1;
             foreach (SwitchingBlocks block in blocksInTurns[currentTurn])
             {
+                if (blocksInTurns.Count == 1 && block.gameObject.activeSelf == true) break;
+                block.gameObject.SetActive(true);
                 StartCoroutine(Blink(block.GetComponent<SpriteRenderer>(), false));
             }
+            yield return new WaitForSecondsRealtime(intervalTime*timeBeforeSwitchPercentage);
         }
     }
     IEnumerator Blink(SpriteRenderer sr, bool hide)
     {
+        if (!hide) SetComponents(sr.gameObject, false);
         float timeForEveryBlink = (intervalTime*timeBeforeSwitchPercentage)/numberOfBlinks;
-        int alpha = hide ? 1 : 0;
+        int originalAlpha = hide ? 1 : 0;
+        int finalAlpha = hide ? 0 : 1;
         for (int i = 0; i < numberOfBlinks; i++) 
         {
             if (i % 2 == 0) sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, blinkAlpha);
-            else sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, alpha);
+            else sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, originalAlpha);
             yield return new WaitForSecondsRealtime(timeForEveryBlink);
         }
-        if (hide) sr.gameObject.SetActive(false);
-        else sr.gameObject.SetActive(true);
+        if (hide)
+        {
+            sr.gameObject.SetActive(false);
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, finalAlpha);
+        }
+        else
+        {
+            sr.gameObject.SetActive(true);
+            SetComponents(sr.gameObject,true);
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, finalAlpha);
+        }
     }
+
+    void SetComponents(GameObject target,bool enabling)
+    {
+        string[] targetComponentsToDisable = target.GetComponent<SwitchingBlocks>().componentsToDisable;
+        foreach (string component in targetComponentsToDisable)
+        {
+            Type typeA = Type.GetType("UnityEngine." + component + ", UnityEngine");
+            Type typeB = Type.GetType(component);
+            if (typeA != null) EnableComponent(target,typeA, enabling);
+            else if (typeB != null) EnableComponent(target, typeB, enabling);
+            else GF.DebugFedeError("No se encontró el tipo: " + component);
+        }
+    }
+    private void EnableComponent(GameObject target,Type type, bool enabling)
+    {
+        // Agarrar el componente de este gameObject
+        Component comp = target.GetComponent(type);
+
+        if (comp != null)
+        {
+            // Agarrar la propiedad enabled del tipo de componente
+            var enabledProperty = type.GetProperty("enabled");
+            if (enabledProperty != null)
+            {
+                // Desactivar o activar el componente
+                enabledProperty.SetValue(comp, enabling);
+            }
+            else
+            {
+                GF.DebugFedeError("NO EXISTE PROPIEDAD");
+            }
+        }
+        else GF.DebugFedeError("COMPONENT NULL");
+    }
+
     List<SwitchingBlocks> GetChildrenSwitchingBlocks(GameObject parent)
     {
         List<SwitchingBlocks> list = new List<SwitchingBlocks>();
@@ -104,9 +151,5 @@ public class SwitchingBlocks : MonoBehaviour
             list.Add(child.GetComponent<SwitchingBlocks>());
         }
         return list;
-    }
-    void Update()
-    {
-        
     }
 }
