c65cfc5cbf0d13bef864e52b82d4373bb7fbd918
Coolfede97 <coolfede97@gmail.com>
2025-02-13 20:12:29 -0300

    cambios de post procesado

--- Spotball/Assets/Scripts/DestructibleBlock.cs
+++ Spotball/Assets/Scripts/DestructibleBlock.cs
@@ -1,12 +1,21 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DestructibleBlock : MonoBehaviour
 {
+    Material material;
+    [SerializeField] float fadeOutDuration;
+    [SerializeField] float fadeInDuration;
+    [SerializeField] private Ease fadeOutEaseType = Ease.OutCubic;
+    [SerializeField] private Ease fadeInEaseType = Ease.InCubic;
+    Collider2D _collider;
     void Start()
     {
-        LevelManager.onReloadPlayer += Reset;     
+        _collider = GetComponent<Collider2D>();
+        material = GetComponent<SpriteRenderer>().material;
+        LevelManager.onReloadPlayer += Reset;
     }
     void Update()
     {
@@ -15,11 +24,21 @@ public class DestructibleBlock : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        gameObject.SetActive(false);
+        if (_collider != null) _collider.enabled = false;
+        material.DOFloat(1, "_FadeAmount", fadeOutDuration).SetEase(fadeOutEaseType).onComplete = ()=> 
+        {
+            GameObject? player = GameObject.FindGameObjectWithTag("Player");
+            if (player!=null && player.GetComponent<PlayerMovement>().shot)
+            {
+                gameObject.SetActive(false);
+            }
+        };
     }
     private void Reset()
     {
         gameObject.SetActive(true);
+        if (_collider != null) _collider.enabled = true;
+        material.DOFloat(0, "_FadeAmount", fadeInDuration).SetEase(fadeInEaseType);
     }
     private void OnDestroy()
     {
